a5c1e83c8bb7ce67fb78be57afa0165c61773481
isabelacolling <collingisabela@gmail.com>
2023-09-26 22:28:19 -0300

    criação terminal e sistema

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite seu primeiro nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        String nomeCliente = nome + " " + sobrenome;
+
+        System.out.println("Digite o número da sua conta");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite o número da sua agência");
+        String agencia = scanner.next();
+        
+        System.out.println("Digite o seu saldo atual");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + "!");
+        System.out.println("Obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", sua conta bancária é " + numero + " e seu saldo de R$" + saldo + " já está disponível para saque.");
+
+        
     }
 }
---
+++ ContaBanco/src/SistemaConta.java
@@ -0,0 +1,11 @@
+public class SistemaConta {
+
+    public String nomeCliente;
+
+    public int numero;
+
+    public String agencia;
+
+    public double saldo;
+
+}
